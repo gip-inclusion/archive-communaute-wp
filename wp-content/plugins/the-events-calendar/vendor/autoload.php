09e72aca912237b9c4045595168359b19c688a2f
hellodeloo <hello@hellodeloo.com>
2021-10-21 10:58:17 +0200

    updates plugins

--- wp-content/plugins/the-events-calendar/vendor/autoload.php
+++ wp-content/plugins/the-events-calendar/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitf90bb32e3429e9aaecc1dee00283fe5b::getLoader();
+return ComposerAutoloaderInit5e1b16a26f3e033eb8d704a7f599045b::getLoader();
