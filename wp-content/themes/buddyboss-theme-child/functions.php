cc4de43ba0e782010b28339fa77d8fa13d625771
hellodeloo <hello@hellodeloo.com>
2021-11-05 10:00:06 +0100

    update email from address

--- wp-content/themes/buddyboss-theme-child/functions.php
+++ wp-content/themes/buddyboss-theme-child/functions.php
@@ -12,3 +12,15 @@ function custom_login_style() {
   wp_enqueue_style('child-login', get_stylesheet_directory_uri() . '/assets/css/login.css', array('buddyboss-theme-login'));  
 }
 add_action('login_enqueue_scripts', 'custom_login_style');
+
+
+function custom_mail_from($email) {
+  return 'noreply@inclusion.beta.gouv.fr';
+}
+add_filter('wp_mail_from', 'custom_mail_from');
+
+
+// function custom_mail_from_name($name) {
+//   return 'My Name';
+// }
+// add_filter('wp_mail_from_name', 'custom_mail_from_name');
