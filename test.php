8e99cdb27f7f10498e09f1e7b8275cd047e4d444
hellodeloo <hello@hellodeloo.com>
2021-11-05 09:51:11 +0100

    update event calendar plugin

--- test.php
+++ test.php
@@ -1,16 +1,15 @@
 <?php
-	/*
-	* Enable error reporting
-	*/
-	ini_set( 'display_errors', 1 );
-	error_reporting( E_ALL );
+// Enable error reporting
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
 
 
+// Test php mail()
 if ( function_exists( 'mail' ) ) {
-			echo "mail() est  disponible";
-	} else {
-			echo "mail() n'est pas disponible";
-	}
+	echo "mail() est  disponible";
+} else {
+	echo "mail() n'est pas disponible";
+}
 
 echo "<br><br>";
 
@@ -28,5 +27,35 @@ if(isset($_GET['from'])) {
 	}		
 } else {
 	echo "Pour envoyer un email test, il faut renseiger les 2 variables <i>from</i> et <i>to</i> dans l'url et recharger la page. <br>Exemple: https://communaute.inclusion.beta.gouv.fr/test.php?from=email@inclusion.beta.gouv.fr&to=davidwatrelot@gmail.com";
+}
+
+
+
+
+echo "<br><br>";
+
+
+
+
+// Test wp_mail()
+$to = "davidwatrelot@gmail.com";
+$subject = 'wp_mail function test';
+$message = 'This is a test of the wp_mail function: wp_mail is working';
+$headers = '';
+ 
+// Load WP components, no themes.
+define('WP_USE_THEMES', false);
+require('wp-load.php');
+ 
+// send test message using wp_mail function.
+$sent_message = wp_mail( $to, $subject, $message, $headers );
+
+//display message based on the result.
+if ( $sent_message ) {
+	// The message was sent.
+	echo 'The test message was sent to davidwatrelot@gmail.com. Check your email inbox.';
+} else {
+	// The message was not sent.
+	echo 'The message was not sent!';
 }
 ?>
