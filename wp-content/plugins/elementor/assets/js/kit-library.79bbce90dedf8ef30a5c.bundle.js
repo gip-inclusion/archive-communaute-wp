0650d6a9410aa11009ece446c7a7bae3d4ecf7fc
hellodeloo <hello@hellodeloo.com>
2021-12-20 15:22:51 +0100

    feat: update elementor one more time

--- wp-content/plugins/elementor/assets/js/kit-library.79bbce90dedf8ef30a5c.bundle.js
+++ wp-content/plugins/elementor/assets/js/kit-library.79bbce90dedf8ef30a5c.bundle.js
@@ -1,4 +1,4 @@
-/*! elementor - v3.5.0 - 12-12-2021 */
+/*! elementor - v3.5.1 - 20-12-2021 */
 (self["webpackChunkelementor"] = self["webpackChunkelementor"] || []).push([["kit-library"],{
 
 /***/ "../core/app/modules/kit-library/assets/js/components/badge.scss":
