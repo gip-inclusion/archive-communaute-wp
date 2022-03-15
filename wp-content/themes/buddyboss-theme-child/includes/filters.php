ca333ff17b0341acbbfc3fc0f3af5bb4a5fbfb46
Enguerran Weiss <enguerranweiss@gmail.com>
2022-03-15 10:56:07 +0100

    modif trad groupes

--- wp-content/themes/buddyboss-theme-child/includes/filters.php
+++ wp-content/themes/buddyboss-theme-child/includes/filters.php
@@ -2,3 +2,10 @@
 
 // Remove bb_core_remove_unfiltered_html that Buddyboss added on group description
 remove_filter( 'bp_get_group_description', 'bb_core_remove_unfiltered_html', 99 );
+
+// function itou_bp_nouveau_customizer_controls($array) {
+//   var_dump($array);
+//   die();
+// }
+
+// add_filter('bp_nouveau_customizer_controls', 'itou_bp_nouveau_customizer_controls', 20);
