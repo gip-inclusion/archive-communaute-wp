d13717e1799c45c298450469f6f96a6254beaf6a
Enguerran Weiss <enguerranweiss@gmail.com>
2022-03-15 15:34:23 +0100

    fix remove action bbp_new_topic

--- wp-content/themes/buddyboss-theme-child/includes/filters.php
+++ wp-content/themes/buddyboss-theme-child/includes/filters.php
@@ -3,4 +3,4 @@
 // Remove bb_core_remove_unfiltered_html that Buddyboss added on group description
 remove_filter( 'bp_get_group_description', 'bb_core_remove_unfiltered_html', 99 );
 
-remove_action( 'bbp_new_topic', 'bbp_notify_forum_subscribers', 11, 4 );
+remove_action( 'bbp_new_topic', 'bbp_notify_forum_subscribers', 11);
