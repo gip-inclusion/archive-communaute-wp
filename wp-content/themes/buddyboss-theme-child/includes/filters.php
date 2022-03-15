29e06dd51f43618b246317d9999d3ff27432a47a
Enguerran Weiss <enguerranweiss@gmail.com>
2022-03-15 12:14:48 +0100

    edit og / twitter url

--- wp-content/themes/buddyboss-theme-child/includes/filters.php
+++ wp-content/themes/buddyboss-theme-child/includes/filters.php
@@ -3,9 +3,6 @@
 // Remove bb_core_remove_unfiltered_html that Buddyboss added on group description
 remove_filter( 'bp_get_group_description', 'bb_core_remove_unfiltered_html', 99 );
 
-// function itou_bp_nouveau_customizer_controls($array) {
-//   var_dump($array);
-//   die();
-// }
+remove_action( 'bbp_new_topic', 'bbp_notify_forum_subscribers', 11, 4 );
 
 // add_filter('bp_nouveau_customizer_controls', 'itou_bp_nouveau_customizer_controls', 20);
