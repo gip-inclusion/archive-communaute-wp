e2c952ee74b39e3aeeceb787ffcd92754131eaed
Enguerran Weiss <enguerranweiss@gmail.com>
2022-03-29 17:13:07 +0200

    fix filtered HTML on group descriptions

--- wp-content/themes/buddyboss-theme-child/includes/filters.php
+++ wp-content/themes/buddyboss-theme-child/includes/filters.php
@@ -2,5 +2,6 @@
 
 // Remove bb_core_remove_unfiltered_html that Buddyboss added on group description
 remove_filter( 'bp_get_group_description', 'bb_core_remove_unfiltered_html', 99 );
+remove_filter( 'groups_group_description_before_save', 'bb_core_remove_unfiltered_html', 99 );
 
 remove_action( 'bbp_new_topic', 'bbp_notify_forum_subscribers', 11);
