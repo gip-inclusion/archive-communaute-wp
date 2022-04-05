3b6e0f63004b270575ae8f6aa51ce885f8703e3a
Enguerran Weiss <enguerranweiss@gmail.com>
2022-04-05 09:41:28 +0200

    idea push trad

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1649682552;s:7:"authKey";s:64:",~%eZ!B2e4udHDUE5)U;tk19k5hruG)lt$S4Ag7@>;U$_>RZ.%&!uXri.X[GmTJO";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2389;}
