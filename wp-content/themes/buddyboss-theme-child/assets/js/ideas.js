8615fe6dd0d2458a1676dacd7d58d22360b9ca89
Enguerran Weiss <enguerranweiss@gmail.com>
2022-04-19 16:06:31 +0200

    rm logs

--- wp-content/themes/buddyboss-theme-child/assets/js/ideas.js
+++ wp-content/themes/buddyboss-theme-child/assets/js/ideas.js
@@ -11,17 +11,12 @@ function changeDateLabel() {
 function onIdeasThumbnailClick() {
   var check = $('.ideapush-container').length > -1;
   var y = 0;
-  console.log('onIdeasThumbnailClick');
-  console.log($('.ideapush-container'), check);
   if(check) {
-    console.log('check');
     // Hack: when user clicks on a service, scroll the window so PAFE triggers update
     $(document).on('click', '.image_picker_selector > li', function(event) {      
       event.preventDefault();
       y = y ? 0:1;
-      console.log('click');
       setTimeout(function() {
-        console.log('scrolling to ', y);
         window.scrollTo(0,y);
       }, 500);
     });
