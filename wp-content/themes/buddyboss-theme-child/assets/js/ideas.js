758f18017d3d3f65e9514ca639100f7d29b8c639
Enguerran Weiss <enguerranweiss@gmail.com>
2022-04-19 10:36:05 +0200

    test fix ideapush

--- wp-content/themes/buddyboss-theme-child/assets/js/ideas.js
+++ wp-content/themes/buddyboss-theme-child/assets/js/ideas.js
@@ -8,6 +8,18 @@ function changeDateLabel() {
   }
 }
 
+function onIdeasThumbnailClick() {
+  var $selector = $('.image_picker_selector .thumbnail');
+  var check = $('.ideapush-container').length > -1;
+  if(check) {
+    // Hack: when user clicks on a service, scroll the window so PAFE triggers update
+    $selector.on('click', function(event) {
+      event.preventDefault();
+      window.scrollTo(0,0);
+    });
+  }
+}
+
 // init
 jQuery(document).ready(function() {
   changeDateLabel();
