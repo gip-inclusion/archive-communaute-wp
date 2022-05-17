eb66f5d836a66c56c19b32e09834f8c3f9e52020
Enguerran Weiss <enguerranweiss@gmail.com>
2022-05-17 15:56:22 +0200

    change select set value method

--- wp-content/themes/buddyboss-theme-child/assets/js/register.js
+++ wp-content/themes/buddyboss-theme-child/assets/js/register.js
@@ -7,23 +7,22 @@ function editRegisterForm() {
     $selector.append('<div class="bp-checkbox-wrap"><input type="checkbox" id="select-member-type-toggle" value="Je souhaite rejoindre la bande à ITOU" class="bs-styled-checkbox"><label for="select-member-type-toggle" class="option-label">Oui, je souhaite devenir ambassadeur de la bande à ITOU !</label></div>')
     var $checkbox = $selector.find('#select-member-type-toggle');
     updateOriginalSelect($checkbox, $originalInput);
-    $checkbox.on('change', function() {
-      var $this = $(this);
+    $checkbox.get(0).addEventListener('click', function(event) {
+      var $this = $(event.currentTarget);
+      console.log('click', $this.is(':checked'));
       updateOriginalSelect($this, $originalInput);
       $originalInput.trigger('change');
+      
     })
   }
 }
 
 function updateOriginalSelect($this, $originalInput) {
-  $originalInput.find('option').removeAttr('selected');
-  $originalInput.find('option[value="481"]').attr('selected', 'selected');
-  if($this.is(':checked')) {
-    $originalInput.find('option').removeAttr('selected');
-    $originalInput.find('option[value="4736"]').attr('selected', 'selected');
-  }
-  console.log($originalInput.find('option:selected').text(), $originalInput.find('option:selected').val());
+  var value = $this.is(':checked') ? "4736":"481";
+  $originalInput.val(value);
+  console.log($originalInput.find('option[value="4736"]'), $originalInput.find('option[value="481"]'),  $originalInput.val(), $originalInput.find('option:selected').val());
 }
+
 
 // init
 jQuery(document).ready(function() {
