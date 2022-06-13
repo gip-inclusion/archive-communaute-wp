5d1ca9a69c9940c2853a2c744ab44a2e38f3ea8e
Enguerran Weiss <enguerranweiss@gmail.com>
2022-06-13 15:14:00 +0200

    redirect to home on logout

--- wp-content/themes/buddyboss-theme-child/includes/filters.php
+++ wp-content/themes/buddyboss-theme-child/includes/filters.php
@@ -23,3 +23,9 @@ remove_filter( 'bp_get_group_description', 'bb_core_remove_unfiltered_html', 99 
 remove_filter( 'groups_group_description_before_save', 'bb_core_remove_unfiltered_html', 99 );
 
 remove_action( 'bbp_new_topic', 'bbp_notify_forum_subscribers', 11);
+
+add_action('wp_logout','itou_redirect_after_logout');
+function itou_redirect_after_logout(){
+  wp_safe_redirect( home_url() );
+  exit();
+}
