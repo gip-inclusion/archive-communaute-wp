447a8f27a51b66e6b408bca7e18cd7c7f03d51aa
hellodeloo <hello@hellodeloo.com>
2023-07-17 10:57:48 +0200

    fix: delete index redirect

--- wp-content/themes/buddyboss-theme-child/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<head>
-		<title>Documentation de l'inclusion - Redirection vers https://aide.emplois.inclusion.beta.gouv.fr</title>
-		<meta http-equiv="refresh" content="0; URL=https://aide.emplois.inclusion.beta.gouv.fr">
-	</head>
-	<body>
-		Redirection vers https://aide.emplois.inclusion.beta.gouv.fr
-	</body>
-</html>
